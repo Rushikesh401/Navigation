56eb88687c61a312978ac6b253fdddb6e99c77ce
Rushh <rushikesh.suradkar@quintessential.design>
2026-06-21 18:44:38 +0530

    Initial Commit

---
+++ Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Rushh on 21/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
